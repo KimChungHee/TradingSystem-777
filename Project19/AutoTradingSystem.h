52914f150e5b6748372b5ffbdbf48c859264dcd9
yujung <unichoi92@hanmail.net>
2025-04-15 15:36:52 +0900

    kiwer, nemo
    login, buy, sell, getPrice

--- Project19/AutoTradingSystem.h
+++ Project19/AutoTradingSystem.h
@@ -1,10 +1,18 @@
-#pragma once
+﻿#pragma once
 #include <string>
 #include "StockerBroker.h"
+using namespace std;
 
 class AutoTradingSystem {
-  StockerBroker *stockBroker;
- public:
-  void selectStockBroker(std::string nameOfStockBroker);
-  std::string getStockBroker();
+	StockerBroker* stockBroker;
+
+public:
+	AutoTradingSystem(StockerBroker* stockBroker);
+
+	void selectStockBroker(string nameOfStockBroker);
+	string getStockBroker();
+	void login(string id, string pass);
+	bool buy(string code, int price, int amount);
+	bool sell(string code, int price, int amount);
+	int getPrice(string code);
 };
